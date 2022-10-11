487c50905b4ce3ba789a03faf98db31c5ee74ccd
Marqui <98498681+Marqui-13@users.noreply.github.com>
2022-10-10 23:54:18 -0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -66,13 +66,13 @@ window.addEventListener('load', function(){
             }, false);
             
             window.addEventListener("touchmove", event => {
-                event.preventDefault();
+                //event.preventDefault();
                 this.mouse.x = event.targetTouches[0].clientX;
                 this.mouse.y = event.targetTouches[0].clientY;
             }, false);
             
             window.addEventListener("touchend", event => {
-                event.preventDefault();
+                //event.preventDefault();
                 this.mouse.x = 0;
                 this.mouse.y = 0;
             }, false);
